14c0788b231f7bff21a38b7c964df432c0e19c2f
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2017-12-14 21:07:59 +0100

    probleme injection balises html avec prepend

--- chatBox/assets/scripts/client.js
+++ chatBox/assets/scripts/client.js
@@ -2,15 +2,24 @@ var socket = io.connect('http://localhost:8080');
 var pseudo = prompt('What\'s your name ?');
 socket.emit('newUser', pseudo);
 
+function insertMsg(targetId, msg){
+  document.getElementById(targetId).prepend(msg.exp + " : " + msg.content);
+}
+function insertOwnMsg(targetId, pseudo, msg){
+  document.getElementById(targetId).prepend(pseudo + " : " + msg );
+}
+
 document.getElementById('submitMsg').addEventListener('click', function(event){
+  event.preventDefault();
   msg = document.getElementById('message').value;
   socket.emit('msg', msg);
-  event.preventDefault();
+  // console.log(msg);
+  insertOwnMsg('chatZone',pseudo, msg);
   document.getElementById('message').value = '';
 });
 
 socket.on('msg', function(msg){
   console.log(msg.exp);
-  console.log(msg.msg);
-  //document.getElementById('chatZone').innerHTML = "<p>"+ "<strong>" +  + "</strong>" +"</p>"
+  console.log(msg.content);
+  insertMsg('chatZone', msg);
 })
