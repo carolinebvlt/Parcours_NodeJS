b72e490e3e2ce9987f7ad361eb8945bb6f524768
Caroline Bieuvelet <caroline.bieuvelet@gmail.com>
2017-12-15 21:16:09 +0100

    npm init

--- messenger/assets/scripts/client.js
+++ messenger/assets/scripts/client.js
@@ -2,21 +2,6 @@ var socket = io.connect('http://localhost:8080');
 var pseudo = prompt('What\'s your name ?');
 socket.emit('newUser', pseudo);
 
-function insertMsg(targetId, msg){
-  //document.getElementById(targetId).prepend(msg.exp + " : " + msg.content);
-  var parent = document.getElementById(targetId);
-  var p = document.createElement('p');
-  p.innerHTML = "<strong>"+msg.exp+" : </strong>" + msg.content;
-  parent.append(p);
-}
-function insertOwnMsg(targetId, pseudo, msg){
-  //document.getElementById(targetId).prepend(pseudo + " : " + msg );
-  var parent = document.getElementById(targetId);
-  var p = document.createElement('p');
-  p.innerHTML = "<strong><i>Me</i> : </strong>" + msg;
-  parent.append(p);
-}
-
 document.getElementById('submitMsg').addEventListener('click', function(event){
   event.preventDefault();
   msg = document.getElementById('message').value;
@@ -27,7 +12,30 @@ document.getElementById('submitMsg').addEventListener('click', function(event){
 });
 
 socket.on('msg', function(msg){
-  console.log(msg.exp);
-  console.log(msg.content);
   insertMsg('chatZone', msg);
 })
+
+socket.on('userON', function(pseudo){
+  ONlog(pseudo);
+})
+
+/*--------------------------  FUNCTIONS  ---------------------------------*/
+
+function insertMsg(targetId, msg){
+  var parent = document.getElementById(targetId);
+  var p = document.createElement('p');
+  p.innerHTML = "<strong>"+msg.exp+" : </strong>" + msg.content;
+  parent.append(p);
+}
+function insertOwnMsg(targetId, pseudo, msg){
+  var parent = document.getElementById(targetId);
+  var p = document.createElement('p');
+  p.innerHTML = "<strong><i>Me</i> : </strong>" + msg;
+  parent.append(p);
+}
+function ONlog(pseudo){
+  var parent = document.getElementById('chatZone');
+  var p = document.createElement('p');
+  p.innerHTML = "<i>"+pseudo+" is online </i>";
+  parent.append(p);
+}
--- messenger/server.js
+++ messenger/server.js
@@ -1,7 +1,7 @@
 var express = require ('express');
 var app = express();
 var server = require('http').createServer(app);
-// var fs = require('fs');
+var fs = require('fs');
 var io = require('socket.io').listen(server);
 
 app.use(express.static(__dirname + '/assets'));
@@ -11,15 +11,13 @@ app.get('/', function(rq, rs){
 
 
 io.sockets.on('connection', function(socket, pseudo){
-  console.log('1 connexion');
 
   socket.on('newUser', function(pseudo){
     socket.pseudo = pseudo;
-    console.log(socket.pseudo);
+    socket.broadcast.emit('userON', pseudo);
   });
 
   socket.on('msg', function(msg){
-    console.log(socket.pseudo + ' : ' + msg);
     socket.broadcast.emit('msg', {exp:socket.pseudo, content:msg});
   })
 });
